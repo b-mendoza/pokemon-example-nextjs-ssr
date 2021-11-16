41f60eac7036a8dfd738691c59407e83b1d77066
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-16 13:14:08 -0600

    feat: update types of `pokemon` API route

--- pages/api/pokemon.ts
+++ pages/api/pokemon.ts
@@ -2,15 +2,16 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 import pokemonList from 'pokemon.json';
 
+import { GetPokemonAPIResponse } from 'typings/api';
 import { Pokemon } from 'typings/pokemon';
 
-type Data = Pokemon | { message: string };
+type Data = GetPokemonAPIResponse;
 
 export default function getPokemon(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  if (!req.query.name) {
+  if (!req.query.name || typeof req.query.name !== 'string') {
     res.status(400).json({ message: '`name` param was not provided' });
 
     return;
@@ -28,7 +29,7 @@ export default function getPokemon(
     .status(pokemon ? 200 : 404)
     .json(
       pokemon
-        ? pokemon
-        : { message: `Pokemon ${req.query.name as string} - Not Found` },
+        ? { pokemon }
+        : { message: `Pokemon ${req.query.name} - Not Found` },
     );
 }
