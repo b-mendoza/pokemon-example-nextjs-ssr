8a046c20b380cbf26c867032dde54a8b0152bc28
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-11-16 15:59:18 -0600

    feat: improve `PokemonView` page component

--- pages/pokemon/[name].tsx
+++ pages/pokemon/[name].tsx
@@ -1,21 +1,20 @@
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 import Link from 'next/link';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+
+import PokemonData from 'components/PokemonData';
 
 import { getPokemon } from 'services/getPokemon';
 
 import { GetPokemonAPIResponse } from 'typings/api';
-
-import { formatPokemonName } from 'utils/formatPokemonName';
 
 type PokemonViewProps = GetPokemonAPIResponse;
 
 function PokemonView(props: PokemonViewProps) {
   if ('message' in props) return <h1>{props.message}</h1>;
 
-  const { base, name } = props.pokemon;
+  const { name } = props.pokemon;
 
   return (
     <>
@@ -25,47 +24,7 @@ function PokemonView(props: PokemonViewProps) {
 
       <main>
         <Container>
-          <>
-            <Row className="mb-3">
-              <h1>{name.english}</h1>
-            </Row>
-
-            <Row className="mb-3">
-              {base
-                ? Object.entries(base).map(([propertie, value], index) => (
-                    <Col
-                      className="d-grid gap-4"
-                      key={index}
-                      md={4}
-                      sm={6}
-                      xs={12}
-                    >
-                      <div className="d-flex justify-content-between">
-                        <p>{propertie}</p>
-
-                        <p>
-                          <strong>{value}</strong>
-                        </p>
-                      </div>
-                    </Col>
-                  ))
-                : null}
-            </Row>
-
-            {name?.english ? (
-              <section className="imageContainer">
-                <Image
-                  alt={name.english}
-                  height={248}
-                  layout="responsive"
-                  priority
-                  src={`/pokemon/${formatPokemonName(name.english)}.jpg`}
-                  title={name.english}
-                  width={248}
-                />
-              </section>
-            ) : null}
-          </>
+          <PokemonData {...props.pokemon} />
 
           <footer>
             <Link href="/">
@@ -82,12 +41,6 @@ function PokemonView(props: PokemonViewProps) {
       <style jsx>{`
         main {
           padding: 1.5rem;
-        }
-
-        section.imageContainer {
-          margin: 0 auto;
-
-          max-width: 40rem;
         }
       `}</style>
     </>
